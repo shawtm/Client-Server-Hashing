63aaa8a2c9f93d4cc6f76f9a993f2087c35ff218
Thomas Shaw <shawtm@rams.colostate.edu>
2018-03-07 11:01:15 -0700

    checkpoint

--- src/cs455/scaling/node/Client.java
+++ src/cs455/scaling/node/Client.java
@@ -1,10 +1,58 @@
 package cs455.scaling.node;
 
+import java.nio.ByteBuffer;
+import java.security.NoSuchAlgorithmException;
+import java.util.LinkedList;
+import java.util.Random;
+
+import cs455.scaling.util.WorkUnit;
+
 public class Client {
-
+	private int messages;
+	private LinkedList<String> hashes;
+	private ByteBuffer buf;
+	private Random r;
+	
+	public Client(String ip, int port, int messages){
+		this.messages = messages;
+		buf = ByteBuffer.allocate(8192); //8kb size for buffer
+		this.r = new Random();
+	}
+	public void start() throws InterruptedException{
+		while(true){
+			// fill buffer with bytes
+			this.fillBuf();
+			// send buffer
+			this.sendBuf();
+			// sleep
+			Thread.sleep((1000/messages));
+		}
+		
+	}
+	public void fillBuf(){
+		while(buf.hasRemaining()){
+			buf.put((byte) r.nextInt(128));
+		}
+		try {
+			hashes.add(WorkUnit.SHA1FromBytes(buf.array()));
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+	}
+	public void sendBuf(){
+		// TODO
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		if (args.length !=3){
+			System.out.println("[ERROR] invalid number of arguments");
+			System.out.println("Use the form client <ip> <port> <r>");
+			return;
+		}
+		Client c = new Client(args[0], Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+		try {
+			c.start();
+		} catch (InterruptedException e) {
+		}
 	}
 
 }
--- src/cs455/scaling/util/WorkUnit.java
+++ src/cs455/scaling/util/WorkUnit.java
@@ -1,12 +1,17 @@
 package cs455.scaling.util;
 
+import java.io.IOException;
 import java.math.BigInteger;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class WorkUnit implements Runnable {
-	
-	
+	private SocketChannel channel;
+	public WorkUnit(SocketChannel channel){
+		this.channel = channel;
+	}
 	public static String SHA1FromBytes(byte[] data) throws NoSuchAlgorithmException {
 		 MessageDigest digest = MessageDigest.getInstance("SHA1");
 		 byte[] hash = digest.digest(data);
@@ -17,6 +22,22 @@ public class WorkUnit implements Runnable {
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		
+		// Read in from channel
+		ByteBuffer buf = ByteBuffer.allocate(8192);
+		int read = 0;
+		while (buf.hasRemaining() && read != -1) {
+			try {
+				read = channel.read(buf);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		// calculate hash
+		try {
+			String hash = SHA1FromBytes(buf.array());
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		// send hash
 	}
 }
