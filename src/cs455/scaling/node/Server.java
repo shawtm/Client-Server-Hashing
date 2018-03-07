f6fabcb5b5067c80effab56cc6c701582d36ec2a
Tom Shaw <31258999+shawtm@users.noreply.github.com>
2018-03-07 09:49:48 -0700

    Checkpoint
    Checkpoint so i can transfer to my laptop

--- src/cs455/scaling/node/Server.java
+++ src/cs455/scaling/node/Server.java
@@ -1,19 +1,34 @@
 package cs455.scaling.node;
 
+import java.io.IOException;
+import java.nio.channels.Selector;
+
 import cs455.scaling.util.BlockingList;
 import cs455.scaling.util.Threadpool;
 
 public class Server {
 	private BlockingList list;
 	private Threadpool pool;
+	private Selector selector;
 	
-	public Server(int threadPoolSize){
+	public Server(int port, int threadPoolSize){
 		list = new BlockingList();
 		pool = new Threadpool(threadPoolSize, list);
+		try {
+			selector = Selector.open();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	public void start(){
+		
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		if (args.length != 2){
+			System.out.println("incorrect Number of arguments");
+		}
+		Server s = new Server(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
 	}
 
 }
--- src/cs455/scaling/util/Threadpool.java
+++ src/cs455/scaling/util/Threadpool.java
@@ -5,6 +5,12 @@ public class Threadpool {
 	
 	public Threadpool(int size, BlockingList list){
 		pool = new Thread[size];
-		
+		for (Thread t: pool)
+			t = new Thread(new Worker(list));
+		this.startThreads();
+	}
+	private void startThreads(){
+		for (Thread t: pool)
+			t.start(); 
 	}
 }
